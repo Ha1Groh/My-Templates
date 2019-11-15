6d3515a00d28342a92dcddf4453eb986e1dce210
Ha1Groh <57674297+Ha1Groh@users.noreply.github.com>
2019-11-15 21:46:03 +0100

    Add files via upload

---
+++ RG-deploy.ps1
@@ -0,0 +1,11 @@
+﻿
+$templateUri = "https://csb37772f1968dfx4eb5xbc1.blob.core.windows.net/case2-deploy/RG-deploy-template.json"
+$parameterUri = "https://csb37772f1968dfx4eb5xbc1.blob.core.windows.net/case2-deploy/RG-deploy-parameters.json"
+  New-AzResourceGroupDeployment `
+  -ResourceGroupName RG `
+  -TemplateUri $templateUri `
+  -TemplateParameterUri $parameterUri `
+
+
+
+
